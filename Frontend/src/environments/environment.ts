98a406a5cdc67dbf114d67b7811528374f92ff27
OumouMohamedBa <24408@esp.mr>
2026-05-09 22:30:03 +0200

    ansible

--- Frontend/src/environments/environment.ts
+++ Frontend/src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost:8001/api',
+  apiUrl: 'http://10.241.109.60:5234/api',
   appName: 'EcoTrack',
   version: '1.0.0'
 };
